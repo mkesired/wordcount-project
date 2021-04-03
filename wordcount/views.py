fe4e7d69ca5f20e57b33e8fed0738f1265236c09
mkesired <manojreddykesi@gmail.com>
2021-04-03 22:04:00 +0530

    Word count project first commit

---
+++ wordcount/views.py
@@ -0,0 +1,24 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+import operator
+
+def home(request):
+    return render(request,"home.html")
+
+def count(request):
+    fulltext = request.GET['fulltext']
+    wordlist = fulltext.split()
+    count=len(wordlist)
+    worddict = {}
+    for word in wordlist:
+        if word in worddict:
+            worddict[word] += 1
+        else:
+            worddict[word]=1
+    sorteddict= sorted(worddict.items(),key=operator.itemgetter(1),reverse=True)
+
+
+    return render(request,"count.html",{"count":count,"sorteddict":sorteddict})
+
+def about(request):
+    return render(request,"about.html")
